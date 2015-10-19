7a79989e38f61fc45463d48994ce054401319d74
aldavies <aldavies@umich.edu>
2015-10-18 20:01:16 -0400

    index update

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 
+var total = 1000;
 var bankroll = 1000;
 var betAmount = 0;
 var playercards = [];
@@ -7,15 +8,15 @@ var playercardsvalue = 0;
 var computercardsvalue= 0;
 // ====================================================
 
-deck =["Ahearts", "Adiamonds", "Aclubs", "Aspades", "2hearts", "2diamonds", "2clubs", "2spades", "3hearts", "3diamonds", "3clubs", "3spades", "4hearts", "4diamonds", "4clubs", "4spades", "5hearts", "5diamonds", "5clubs", "5spades", "6hearts", "6diamonds", "6clubs", "6spades", "7hearts", "7diamonds", "7clubs", "7spades", "8hearts", "8diamonds", "8clubs", "8spades", "9hearts", "9diamonds", "9clubs", "9spades", "10hearts", "10diamonds", "10clubs", "10spades", "Jhearts", "Jdiamonds", "Jclubs", "Jspades", "Qhearts", "Qdiamonds", "Qclubs", "Qspades", "Khearts", "Kdiamonds", "Kclubs", "Kspades"];
+deck =[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51 ];
 
 values =[11, 11, 11, 11, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 9, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10];
 
 
 
-numericals = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+numericals = [];
 
-suit = ['hearts', 'diamonds', 'clubs', 'spades']
+suit = ["hearts", "diamonds", "clubs", "spades", "hearts", "diamonds", "clubs", "spades", "hearts", "diamonds", "clubs", "spades", "hearts", "diamonds", "clubs", "spades", "hearts", "diamonds", "clubs", "spades", "hearts", "diamonds", "clubs", "spades", "hearts", "diamonds", "clubs", "spades", "hearts", "diamonds", "clubs", "spades", "hearts", "diamonds", "clubs", "spades", "hearts", "diamonds", "clubs", "spades", "hearts", "diamonds", "clubs", "spades", "hearts", "diamonds", "clubs", "spades", "hearts", "diamonds", "clubs", "spades"]
 // ======================================================
 				// To get full deck of cards with suit
 // var deckarray = function() {
@@ -29,6 +30,8 @@ suit = ['hearts', 'diamonds', 'clubs', 'spades']
 // deckarray();
 
 
+
+var win
 // ================================================					
 
 		// To bet a certain amount and start the game
@@ -39,8 +42,7 @@ var startGame = function(){
 	dealcomputer();
 
 	dealplayer();
-	
-
+	hit();
 
 
 });
@@ -71,20 +73,26 @@ var dealplayer = function() {
 
 
 // ==========================================================
-
+					// hit to add another card
 var hit = function() {
 	$('#hit').click(function(){
 		playercards.push(deck[Math.floor(Math.random() * deck.length)]);
-
-		
-
-	
 	})
 };
 
-hit();
+
+// ==============================================
+					// bankroll
 
 
+var trackBankroll = function(){
+	 total = bankroll - betAmount;
+	 $('#bankroll').html(total)
+
+	
+
+}
+trackBankroll();
 
 
 
