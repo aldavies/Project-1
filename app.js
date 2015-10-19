8f99e8c8becc3dc6408a0e1f5a719f5909631e1b
aldavies <aldavies@umich.edu>
2015-10-19 09:17:24 -0400

    blackjack

--- app.js
+++ app.js
@@ -4,8 +4,6 @@ var bankroll = 1000;
 var betAmount = 0;
 var playercards = [];
 var computercards = [];
-var playercardsvalue = 0;
-var computercardsvalue= 0;
 // ====================================================
 
 deck =[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51 ];
@@ -13,8 +11,7 @@ deck =[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
 values =[11, 11, 11, 11, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 9, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10];
 
 
-
-numericals = [];
+facevalues = ["A", "A", "A", "A", "2", "2", "2", "2", "3", "3", "3", "3", "4", "4", "4", "4", "5", "5", "5", "5", "6", "6", "6", "6", "7", "7", "7", "7", "8", "8", "8", "8", "9", "9", "9", "9", "10", "10", "10", "10", "J", "J", "J", "J", "Q", "Q", "Q", "Q", "K", "K", "K", "K"];
 
 suit = ["hearts", "diamonds", "clubs", "spades", "hearts", "diamonds", "clubs", "spades", "hearts", "diamonds", "clubs", "spades", "hearts", "diamonds", "clubs", "spades", "hearts", "diamonds", "clubs", "spades", "hearts", "diamonds", "clubs", "spades", "hearts", "diamonds", "clubs", "spades", "hearts", "diamonds", "clubs", "spades", "hearts", "diamonds", "clubs", "spades", "hearts", "diamonds", "clubs", "spades", "hearts", "diamonds", "clubs", "spades", "hearts", "diamonds", "clubs", "spades", "hearts", "diamonds", "clubs", "spades"]
 // ======================================================
@@ -31,23 +28,43 @@ suit = ["hearts", "diamonds", "clubs", "spades", "hearts", "diamonds", "clubs", 
 
 
 
-var win
+$('#bankroll').text(bankroll);
+$('#bet-button').click(function(){
+	betAmount = parseInt($("#bet-input").val());
+	trackBankroll();
+	startGame();
+});
+$('#stand').click(function(){
+		while(getHandValue(computercards) < 17){
+			compCard = Math.floor(Math.random() * deck.length);
+			computercards.push(deck[compCard]);
+			deck.splice(compCard, 1);
+			$('#card' + computercards.length).text(facevalues[computercards[computercards.length - 1]]);
+			if (isBust(computercards)){
+				alert('Dealers busts!');
+			}
+		}
+	});
+
+var isBust = function(hand){
+	if( getHandValue(hand) > 21){
+		return true;
+	}else{
+		return false;
+	}
+
+}
 // ================================================					
 
-		// To bet a certain amount and start the game
+// To bet a certain amount and start the game
 var startGame = function(){
-	$('#bet-button').one("click", function(){
-	betAmount = parseInt($("#bet-input").val());
 	// deal cards function
 	dealcomputer();
-
 	dealplayer();
 	hit();
 
-
-});
+	
 }
-startGame();
     // });
 // ==================================================
 
@@ -55,8 +72,9 @@ startGame();
 
 var dealcomputer = function() {
 	for (i = 0; i < 2; i++){
-	computercards.push(deck[Math.floor(Math.random() * deck.length)]);
-}
+		computercards.push(deck[Math.floor(Math.random() * deck.length)]);
+		$('#card' + (i + 1)).text(facevalues[computercards[i]]);
+	}
 }
 
 // dealcomputer();
@@ -65,8 +83,8 @@ var dealcomputer = function() {
 
 var dealplayer = function() {
 	for (i = 0; i < 2; i++) {
-
 		playercards.push(deck[Math.floor(Math.random() * deck.length)]);
+		$('#card' + (6 + i)).text(facevalues[playercards[i]]);
 	}
 }
 // dealplayer();
@@ -76,7 +94,17 @@ var dealplayer = function() {
 					// hit to add another card
 var hit = function() {
 	$('#hit').click(function(){
-		playercards.push(deck[Math.floor(Math.random() * deck.length)]);
+	  	card  = Math.floor(Math.random() * deck.length);
+	  	playercards.push(deck[card]);
+		deck.splice(card,1);
+		$('#card' + (5 + playercards.length)).text(facevalues[playercards[playercards.length - 1]]);
+		alert(getHandValue(playercards));
+
+			if (isBust(playercards)){
+				alert('Bust!');
+			}
+
+		
 	})
 };
 
@@ -86,13 +114,30 @@ var hit = function() {
 
 
 var trackBankroll = function(){
-	 total = bankroll - betAmount;
-	 $('#bankroll').html(total)
-
-	
-
+	 bankroll -= betAmount;
+	 $('#bankroll').text(bankroll);
 }
 trackBankroll();
+
+
+
+// var didPlayerWin = function() {
+// 	for (int i = 0; i < playercards.length; i++){
+// 		value = values[playercards[i]];
+// 		suit = suits[playercard[i]];
+// 	}
+// }
+
+var getHandValue = function(hand){
+	var sum = 0;
+	// looping through a players hand to get the sum of values
+	for( i = 0; i < hand.length; i++){
+		sum = sum + values[hand[i]];
+	}
+	return sum;
+
+};
+
 
 
 
