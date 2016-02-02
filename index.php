11af851b96c290fe1a0307af95861105a5b5015c
aldavies <aldavies@umich.edu>
2016-02-02 18:47:07 -0500

    heroku

--- index.php
+++ index.php
@@ -1,6 +1,1 @@
-<?php include_once("home.html"); ?>
-
-<?php
-   //include a javascript file
-   echo "<script type='text/javascript' src='app.js'></script>";
-?>
+$ echo 'php_flag engine off' > .htaccess
