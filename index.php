eb81f2cd9150c1568c26c50134b41671e3123d67
aldavies <aldavies@umich.edu>
2016-02-02 18:49:56 -0500

    hello

--- index.php
+++ index.php
@@ -1,1 +1,6 @@
-$ echo 'php_flag engine off' > .htaccess
+<?php include("home.html"); ?>
+
+<?php
+   //include a javascript file
+   echo "<script type='text/javascript' src='app.js'></script>";
+?>
